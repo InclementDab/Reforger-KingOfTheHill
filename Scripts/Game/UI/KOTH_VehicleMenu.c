9c4eb005ccbac606f2bafc43f8fca156ed374f63
Tyler Paul <tylerpaul2329@gmail.com>
2022-05-22 16:38:30 -0400

    Fixed menu not being loaded properly

--- Scripts/Game/UI/KOTH_VehicleMenu.c
+++ Scripts/Game/UI/KOTH_VehicleMenu.c
@@ -3,24 +3,24 @@ class KOTH_VehicleSpawnerMenu: SCR_SuperMenuBase
 	//------------------------------------------------------------------------------------------------
 	override void OnMenuOpen()
 	{
-
+		super.OnMenuOpen();
 	}
 
 	//------------------------------------------------------------------------------------------------
 	override void OnMenuClose()
 	{
-
+		super.OnMenuClose();
 	}
 
 	//------------------------------------------------------------------------------------------------
 	override void OnMenuInit()
 	{
-
+		super.OnMenuInit();
 	}
 
 	//------------------------------------------------------------------------------------------------
 	override void OnMenuUpdate(float tDelta)
 	{
-
+		super.OnMenuUpdate(tDelta);
 	}
-};
+}
